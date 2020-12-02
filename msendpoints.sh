c58c9641dccb9d024a380bb9cd76f82a8da31bcd
Chriztoffer <ch@ntrv.dk>
2020-12-02 18:07:25 +0000

    Add msendpoints.sh

---
+++ msendpoints.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+uri=https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7
+dir=$(dirname $0)/microsoft-ips
+jsonfile=${dir}/ms-endpoints.json
+
+# Check if dir exists, else create it
+if [ ! -d ${dir} ]; then
+  mkdir -p ${dir}
+fi
+
+# Fetch json file from ms website
+curl -snGL -o ${jsonfile} -z ${jsonfile} ${uri}
+
+for id in $(cat ${jsonfile} | jq '.[] | select(.ips) | .id'); do
+  cat ${jsonfile} |
+    jq ".[] | select(.id == ${id}) | .ips[]" |
+    sed 's/^"//;s/"$//' |
+    tee ${dir}/ms-endpoint-${id}-ips.txt
+done
+
+for id in $(cat ${jsonfile} | jq '.[] | select(.urls) | .id'); do
+  cat ${jsonfile} |
+    jq ".[] | select(.id == ${id}) | .urls[]" |
+    sed 's/^"//;s/"$//' |
+    tee ${dir}/ms-endpoint-${id}-urls.txt
+done
+
+for id in $(cat ${jsonfile} | jq '.[] | select(.tcpPorts) | .id'); do
+  cat ${jsonfile} |
+    jq ".[] | select(.id == ${id}) | .tcpPorts | split(\",\")[]" |
+    sed 's/^"//;s/"$//' |
+    tee ${dir}/ms-endpoint-${id}-tcpports.txt
+done
+
+for id in $(cat ${jsonfile} | jq '.[] | select(.udpPorts) | .id'); do
+cat ${jsonfile} |
+    jq ".[] | select(.id == ${id}) | .udpPorts | split(\",\")[]" |
+    sed 's/^"//;s/"$//' |
+    tee ${dir}/ms-endpoint-${id}-udpports.txt
+done
+
+git add microsoft-ips/*
+git commit -m "Updates files - $(date -uR)"
